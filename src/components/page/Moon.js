7c259f910febe89d29e51643caaa8b27675b78ad
gildydtjd <gildydtjd@naver.com>
2021-07-06 22:00:50 +0900

    #scollfadein

--- src/components/page/Moon.js
+++ src/components/page/Moon.js
@@ -1,15 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
+import useScrollFadeIn from '../../hooks/useScrollFadeIn';
 
 function Moon(props) {
     const MoonDiv = styled.div`
+    height: 100%;
+    width: 100%;
     background-image: url("/assets/img/Moon.jpg");
     background-size: cover;
-    width: 100%;
-    height: 100vh;
+    display: inline-block;
+    background-attachment: fixed;
+    .moon__about{
+        height: 100vh;
+        p{
+            font-size: 100px;
+            color: white;
+            font-weight: 700;
+        }
+    }
+    .moon__about2{
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .moon__rocket{
+        background-image: url("/assets/img/rocket2.png");
+        width: 150px;
+        height: 250px;
+        background-position: center;
+        background-size: cover;
+    }
+        
+        .moon__about2__left{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50%;
+            height: 100vh;
+        }
+        .moon__about2__right{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50%;
+            height: 100vh;
+        }
+    }
+    
     `
+
     return (
         <MoonDiv>
+            <div className="moon__about">
+                <p {...useScrollFadeIn("up",1,1)}>Moon</p>
+            </div>
+            <div className="moon__about2">
+                <div className="moon__about2__left" {...useScrollFadeIn("up",0.5,1)}>
+                <div className="moon__rocket" />
+                </div>
+                <div className="moon__about2__right"  {...useScrollFadeIn("up",0.5,1)}>
+                <div className="moon__rocket" />
+                </div>
+                </div>
         </MoonDiv>
     );
 }
